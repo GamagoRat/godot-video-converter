6aee2d7525077b10a01fc431137740cb67c3454c
SoraxDubbing <52173496+Sorax5@users.noreply.github.com>
2025-11-12 20:22:26 +0100

    Add import options to VideoImporter plugin
    Introduces customizable import options (bitrate, fps, GOP size, quality) to the VideoImporter plugin for video conversion. Updates example scene and import file to use new options and correct resource UID.

--- addons/videoconverter/import/VideoImporter.gd
+++ addons/videoconverter/import/VideoImporter.gd
@@ -1,5 +1,7 @@
-﻿@tool
+@tool
 extends EditorImportPlugin
+
+enum Presets { DEFAULT }
 
 func _get_importer_name() -> String:
 	return "godot.video.importer.videoconverter"
@@ -19,8 +21,59 @@ func _get_resource_type() -> String:
 func _get_priority() -> float:
 	return 1.0
 	
-func _get_import_options(path: String, preset_index: int) -> Array[Dictionary]:
-	return []
+# --- Nombre de presets disponibles ---
+func _get_preset_count() -> int:
+	return Presets.size()
+
+# --- Nom du preset ---
+func _get_preset_name(preset_index: int) -> String:
+	match preset_index:
+		Presets.DEFAULT:
+			return "Default"
+		_:
+			return "Unknown"
+
+# --- Options de l'importeur ---
+func _get_import_options(path: String, preset_index: int) -> Array:
+	match preset_index:
+		Presets.DEFAULT:
+			return [
+				{
+					"name": "video_bitrate",
+					"default_value": 4000,
+					"type": TYPE_INT,
+					"hint": PROPERTY_HINT_RANGE,
+					"hint_string": "100,12000,10",
+					"usage": PROPERTY_USAGE_DEFAULT
+				},
+				{
+					"name": "fps",
+					"default_value": 24,
+					"type": TYPE_INT,
+					"hint": PROPERTY_HINT_RANGE,
+					"hint_string": "1,60,1",
+					"usage": PROPERTY_USAGE_DEFAULT
+				},
+				{
+					"name": "gop_size",
+					"default_value": 64,
+					"type": TYPE_INT,
+					"hint": PROPERTY_HINT_RANGE,
+					"hint_string": "1,300,1",
+					"usage": PROPERTY_USAGE_DEFAULT
+				},
+				{
+					"name": "qv",
+					"default_value": 6,
+					"type": TYPE_INT,
+					"hint": PROPERTY_HINT_RANGE,
+					"hint_string": "1,10,1",
+					"usage": PROPERTY_USAGE_DEFAULT
+				}
+			]
+		_:
+			return []
+
 
 # documentation source https://docs.godotengine.org/fr/4.x/tutorials/animation/playing_videos.html#doc-playing-videos-recommended-theora-encoding-settings
 func _import(source_file: String, save_path: String, options: Dictionary, platform_variants: Array, gen_files: Array) -> int:
@@ -28,18 +81,32 @@ func _import(source_file: String, save_path: String, options: Dictionary, platfo
 	var src := ProjectSettings.globalize_path(source_file)
 	var out_rel := save_path + "." + _get_save_extension()
 	var out_abs := ProjectSettings.globalize_path(out_rel)
-	var args := ["-y", "-i", src, "-q:v", "6", "-g:v", "64", out_abs]
-	var output := []
 	
+	# Récupération des options utilisateur ou valeurs par défaut
+	var bitrate: int = int(options.get("video_bitrate", 4000) * 1000)
+	var fps: int = int(options.get("fps", 24))
+	var gop_size: int = int(options.get("gop_size", 64))
+	var qv: int = int(options.get("qv", 6))
+
+	var args := [
+		"-y", "-i", src,
+		"-b:v", str(bitrate),
+		"-r", str(fps),
+		"-q:v", str(qv),
+		"-g:v", str(gop_size),
+		out_abs
+	]
+	
+	var output := []
 	var exit_code := OS.execute(ffmpeg_path, args, output, true)
-	if exit_code == 0:
-		if FileAccess.file_exists(out_abs):
-			gen_files.append(out_rel);
-			return OK
-		else:
-			push_error("Video conversion failed: output file was not created.")
-			return ERR_CANT_CREATE
-	else:
-		var output_text := PackedStringArray(output);
-		push_error("Video conversion failed with exit code %d. Output:\n%s" % [exit_code, output_text])
+	
+	if exit_code != 0:
+		push_error("Video conversion failed with exit code %d. Output:\n%s" % [exit_code, PackedStringArray(output)])
 		return ERR_CANT_CREATE
+
+	if not FileAccess.file_exists(out_abs):
+		push_error("Video conversion failed: output file was not created.")
+		return ERR_CANT_CREATE
+
+	gen_files.append(out_rel)
+	return OK
